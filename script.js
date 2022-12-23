4bc215d4f659f0110abd4706fd4a14068b6aff91
otiteakpofure <akpofure.otite@gmail.com>
2022-12-23 06:56:22 +0100

    This is just a comment

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-// const second = document.querySelector(".second").textContent;
-// console.log(second)
+const second = document.querySelector(".second").textContent;
+console.log(second)
 
 
 
 
 
-// const findAnswer = function (){
-//     return 5 + 6
-// }
+const findAnswer = function (){
+    return 5 + 6
+}
 
-// console.log(findAnswer())
-// console.log(findAnswer())
-// console.log(findAnswer()) 
+console.log(findAnswer())
+console.log(findAnswer())
+console.log(findAnswer()) 
 
 
-// class Person{
-//     constructor( firstname, lastname){
-//         this.firstname = firstname;
-//         this.lastname= lastname
-//         this.middlename = "jake"
-//     }
-// }
+class Person{
+    constructor( firstname, lastname){
+        this.firstname = firstname;
+        this.lastname= lastname
+        this.middlename = "jake"
+    }
+}
 
-// const firStudent = new Person("james","eka")
-// const secondName = new Person("james","freddy")
-// console.log(firStudent, secondName)
+const firStudent = new Person("james","eka")
+const secondName = new Person("james","freddy")
+console.log(firStudent, secondName)
+
+// using encapscultation for the classes 
+
+// this is for a new day
